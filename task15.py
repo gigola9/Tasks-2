4aff758f6a5a2307a9ac1f20a6674d4bc9a3a44c
Giorgi Asanidze <gasanidze@tbcbank.com.ge>
2021-10-27 20:05:05 +0400

    fix 15

--- task15.py
+++ task15.py
@@ -1,12 +1,11 @@
 numbs = [100, 44.5, 231, 8888, 7.81]
 
-print(sum(numbs))
-print(min(numbs))
-print(max(numbs))
-print(sum(numbs) / len(numbs))
+print('ჯამი ' + str(sum(numbs)))
+print('მინიმალური ' + str(min(numbs)))
+print('მაქსიმალური ' + str(max(numbs)))
+print('საშუალო ' + str(sum(numbs) / len(numbs)))
 
 numbs.append(102)
 numbs[2] = 205
 numbs.pop(3)
 numbs.sort()
-print(numbs)
